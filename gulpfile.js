53fde485ac6227e5583aa3ac8704cc744d90a8d2
Diego <reptile9985@gmail.com>
2016-03-28 21:18:41 +0200

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+﻿var gulp = require("gulp");          // Load gulp
+var uglify = require("gulp-uglify"); // Load gulp-uglify
+var concat = require("gulp-concat"); // Load gulp-concat
+
+gulp.task("combine-and-uglify", function () {
+    return gulp.src('www/scripts/*.js')
+        .pipe(concat('combined.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('min/scripts'));
+});
