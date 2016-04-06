a40f530b00868042a3065e28f5ca03819189047a
Diego ASI <reptile9985@gmail.com>
2016-04-06 12:12:42 +0200

    Add engines

---
+++ src/Controllers/SearchEnginesController.ts
@@ -0,0 +1,14 @@
+﻿///<reference path="../../typings/tsd.d.ts"/>
+
+class SearchEnginesController
+{
+    static $inject = ["Loader"];
+    searchEngines: Array<SearchEngines.ISearchEngine>
+
+    constructor(public Loader: SearchEngines.ILoader)
+    {
+        this.searchEngines = Loader.get();
+    }
+}
+
+angular.module("VisualSearch").controller("SearchEnginesController", SearchEnginesController);
