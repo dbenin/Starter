157f5b1e74fa47ff8543d240a9d638d2bbf47f31
Diego ASI <reptile9985@gmail.com>
2016-04-06 17:50:28 +0200

    Add camera test

---
+++ src/Controllers/CameraController.ts
@@ -0,0 +1,18 @@
+﻿///<reference path="../../typings/tsd.d.ts"/>
+
+class CameraController
+{
+    static $inject = ["Picture"];
+
+    lastPhoto: any;
+
+    constructor(public Picture: Services.Camera.IPicture) { this.lastPhoto = ""; }
+
+    getPhoto()
+    {
+        let options: CameraOptions = {};
+        this.Picture.take(options).then(image => { console.log("CAMERA SUCCESS"); this.lastPhoto = image; }, error => { console.log("CAMERA ERROR"); });
+    }
+}
+
+angular.module("VisualSearch").controller("CameraController", CameraController);
---
+++ src/Services/CameraService.ts
@@ -0,0 +1,25 @@
+﻿///<reference path="../../typings/tsd.d.ts"/>
+
+module Services.Camera
+{
+    export interface IPicture
+    {
+        take(options: CameraOptions): ng.IPromise<any>;
+    }
+
+    export class Picture implements IPicture
+    {
+        static $inject = ["$q"];
+        
+        constructor(private $q: ng.IQService) { }
+
+        take(options)
+        {
+            let q = this.$q.defer();
+            navigator.camera.getPicture((result): void => { q.resolve(result); }, (error): void => { q.reject(error); }, options);
+            return q.promise;
+        }
+    }
+}
+
+angular.module("VisualSearch").service("Picture", Services.Camera.Picture);
