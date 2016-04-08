d1e7eb5a22eb1af834dcd327d6a9694f9c533c19
Diego ASI <reptile9985@gmail.com>
2016-04-08 14:03:14 +0200

    Add angular services needed as parameters

--- src/SearchEngines.ts
+++ src/SearchEngines.ts
@@ -12,36 +12,47 @@ interface ISearchEngine
     key: string;
     sets: Array<ISearchEngineSet>;
     options?: CameraOptions;
-    //search(picture: string): ng.IPromise<any>;
+    //search(picture: string): ng.IPromise<any>;//usato da getResult
+    //getResult(picture: string): IResult;//torna un risultato, da definire risultato generico e specifico per ogni servizio
 }
 
 abstract class SearchEngine implements ISearchEngine
 {
+    //abstract search(picture: string): any;
     constructor(public name: string, public key: string, public sets: Array<ISearchEngineSet>) { }
 }
 
 class CloudSight extends SearchEngine
 {
-    constructor(key: string)
+    private $q: ng.IQService;
+    private $http: ng.IHttpService;
+    private $interval: ng.IIntervalService;
+    constructor(key: string, q: ng.IQService, http: ng.IHttpService, interval: ng.IIntervalService)
     {
         let sets: Array<ISearchEngineSet> = [{ name: "Product" }];
         super("CloudSight", key, sets);
+        this.$q = q;
+        this.$http = http;
+        this.$interval = interval;
     }
 }
 
 class Imagga extends SearchEngine
 {
-    constructor(key: string)
+    private $q: ng.IQService;
+    constructor(key: string, q: ng.IQService)
     {
         let sets: Array<ISearchEngineSet> = [{ name: "Tagging", value: "tagging" }];
         super("Imagga", key, sets);
+        this.$q = q;
     }
 }
 
 class GoogleCloudVision extends SearchEngine
 {
+    private $http: ng.IHttpService;
     options: CameraOptions;
-    constructor(key: string)
+    constructor(key: string, http: ng.IHttpService)
     {
         let sets: Array<ISearchEngineSet> = [
             { name: "Label Detection", value: "LABEL_DETECTION" },
@@ -50,14 +61,16 @@ class GoogleCloudVision extends SearchEngine
             { name: "Text Detection", value: "TEXT_DETECTION" }
         ];
         super("GoogleCloudVision", key, sets);
+        this.$http = http;
         this.options = { destinationType: 0 };//Camera.DestinationType.DATA_URL//Camera is not defined?
     }
 }
 
 class MetaMind extends SearchEngine
 {
+    private $http: ng.IHttpService;
     options: CameraOptions;
-    constructor(key: string)
+    constructor(key: string, http: ng.IHttpService)
     {
         let sets: Array<ISearchEngineSet> = [
             { name: "General Classifier", value: "imagenet-1k-net" },
@@ -65,13 +78,15 @@ class MetaMind extends SearchEngine
             { name: "Custom Classifier", value: "41291" }
         ];
         super("MetaMind", key, sets);
+        this.$http = http;
         this.options = { destinationType: 0 };//Camera.DestinationType.DATA_URL//Camera is not defined?
     }
 }
 
 class JustVisual extends SearchEngine
 {
-    constructor(key: string)
+    private $q: ng.IQService;
+    constructor(key: string, q: ng.IQService)
     {
         let sets: Array<ISearchEngineSet> = [
             { name: "Fashion", value: "http://style.vsapi01.com" },
@@ -80,5 +95,6 @@ class JustVisual extends SearchEngine
             { name: "Pet", value: "http://pets.vsapi01.com" }
         ];
         super("JustVisual", key, sets);
+        this.$q = q;
     }
 }
